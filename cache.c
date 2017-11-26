b7cef4fc822ab1328caab7ad5ff422dd1dbc3fbf
Geoffrey Ndu <IT302@users.noreply.github.com>
2017-11-26 11:31:38 +0000

    Added licensing text to source code.

--- cache.c
+++ cache.c
@@ -1,3 +1,18 @@
+/* 
+ * cache-lkm
+ * 
+ * This is a LKM for disabling CPU caches.
+ *
+ * Copyright (c) 2017 Geoffrey Ndu 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ */
+
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/smp.h>
